e2a446ec80151471c1e2e22b1b2c182399f15b9d
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-01-26 15:12:29 +0100

    Update Smartphone.java

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,7 +1,10 @@
 public class Smartphone extends Phone implements Camera, Clock{
-
+public Smartphone(String brand, String model) {
+        super(brand, model);
+    }
     @Override
     public void alarm() {
+        super.call(676899800);
         System.out.println("The alarm is sounding");
     }
 }
