357ab3fa4086fd1b50e85a6ee370b0db3605a3de
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-01-26 15:11:49 +0100

    Update Phone.java

--- src/Phone.java
+++ src/Phone.java
@@ -1,7 +1,26 @@
 public class Phone {
-    String brand;
-    String model;
+   private String brand;
+   private String model;
 
+    public Phone(String brand, String model){
+       this.brand = brand;
+       this.model = model;
+   }
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
     void call(int numberOfPhone){
         System.out.println(numberOfPhone + "is calling");
     }
