da88f83112b60084d24dddac1d166d192000443d
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-01-26 15:10:36 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Camera.toTakeAPhoto();
-Smartphone RedMi = new Smartphone();
+Smartphone RedMi = new Smartphone("Xiaomi", "Note 13");
 RedMi.alarm();
 //RedMi.call(676899800);
     }
